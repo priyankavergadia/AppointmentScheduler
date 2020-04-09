831519de0212e6be63a3e023aadb3f08b1a34b36
Viral Vyas <zero4cool@gmail.com>
2020-04-09 14:43:42 +0530

    Update index.js
    below code is modified by ViKi Vyas(in English V.K. Vyas), 
     changelog
     1) it can work with any timezone. as old code had problme to work with + timezone.
     2) agent was replying time to near by rounding time e.g 2:30 was rounded to 2:00. appoiment booking was ok but agent reply had problem.
     3) agent reply after booking was made more like human.

--- index.js
+++ index.js
@@ -12,6 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ 
+ below code is modified by ViKi Vyas(in English V.K. Vyas), 
+ changelog
+ 1) it can work with any timezone. as old code had problme to work with + timezone.
+ 2) agent was replying time to near by rounding time e.g 2:30 was rounded to 2:00. appoiment booking was ok but agent reply had problem.
+ 3) agent reply after booking was made more like human. 
  */
 
  'use strict';
@@ -34,28 +40,24 @@
  const calendar = google.calendar('v3');
  process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
  
- const timeZone = 'America/Los_Angeles';
- const timeZoneOffset = '-07:00';
+ const timeZone = '<Your time zone e.g America/Los_Angeles or Asia/Colombo>';
+ const timeZoneOffset = 'Your time + /- to gmt 000 e.g -07:00 or +5:30';
  
  exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
    const agent = new WebhookClient({ request, response });
-   console.log("Parameters", agent.parameters);
    const appointment_type = agent.parameters.AppointmentType
    function makeAppointment (agent) {
      // Calculate appointment start and end datetimes (end = +1hr from start)
-     //console.log("Parameters", agent.parameters.date);
-     const dateTimeStart = new Date(Date.parse(agent.parameters.date.split('T')[0] + 'T' + agent.parameters.time.split('T')[1].split('-')[0] + timeZoneOffset));
-     const dateTimeEnd = new Date(new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1));
-     const appointmentTimeString = dateTimeStart.toLocaleString(
-       'en-US',
-       { month: 'long', day: 'numeric', hour: 'numeric', timeZone: timeZone }
-     );
- 
+    const AppointmentDate = agent.parameters.date.split('T')[0];
+    const AppointmentTime = agent.parameters.time.split('T')[1].substr(0,8);
+    const dateTimeStart = new Date(AppointmentDate + 'T' + AppointmentTime+timeZoneOffset);
+    const dateTimeEnd = new Date(new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1));
+    
      // Check the availibility of the time, and make an appointment if there is time on the calendar
-     return createCalendarEvent(dateTimeStart, dateTimeEnd, appointment_type).then(() => {
-       agent.add(`Ok, let me see if we can fit you in. ${appointmentTimeString} is fine!.`);
+    return createCalendarEvent(dateTimeStart, dateTimeEnd, appointment_type).then(() => {
+       agent.add(`Let me see if we can fit you in on ${AppointmentDate} at ${AppointmentTime}! Yes It is fine!.`);
      }).catch(() => {
-       agent.add(`I'm sorry, there are no slots available for ${appointmentTimeString}.`);
+       agent.add(`I'm sorry, there are no slots available for ${AppointmentDate} at ${AppointmentTime}.`);
      });
    }
  
@@ -63,10 +65,8 @@
    intentMap.set('Schedule Appointment', makeAppointment);
    agent.handleRequest(intentMap);
  });
- 
- 
- 
- function createCalendarEvent (dateTimeStart, dateTimeEnd, appointment_type) {
+
+function createCalendarEvent (dateTimeStart, dateTimeEnd, appointment_type) {
    return new Promise((resolve, reject) => {
      calendar.events.list({
        auth: serviceAccountAuth, // List events for time period
